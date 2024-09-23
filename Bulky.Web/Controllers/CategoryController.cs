8497bdc41aa968229d6f42fd5b3f5cb907593b31
BehruzbekUmarov <bekhruz476@gmail.com>
2024-09-22 19:59:01 -0800

    Add project files.

---
+++ Bulky.Web/Controllers/CategoryController.cs
@@ -0,0 +1,28 @@
+﻿using Bulky.Web.Data;
+using Bulky.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bulky.Web.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public CategoryController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            List<Category> categories = _dbContext.Categories.ToList();
+
+            return View(categories);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ Bulky.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Bulky.Web.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bulky.Web.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Fiction", DisplayOrder = 1},
+                new Category { Id = 2, Name = "Diction", DisplayOrder = 2},
+                new Category { Id = 3, Name = "Section", DisplayOrder = 3}
+                );
+        }
+    }
+}
