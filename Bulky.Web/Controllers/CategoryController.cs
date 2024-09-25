0abf2974f7815e2594acd248b6eb86d358d1fe86
BehruzbekUmarov <bekhruz476@gmail.com>
2024-09-24 18:08:56 -0800

    Category UI is added

--- Bulky.Web/Controllers/CategoryController.cs
+++ Bulky.Web/Controllers/CategoryController.cs
@@ -28,13 +28,64 @@ namespace Bulky.Web.Controllers
         [HttpPost]
         public IActionResult Create(Category category)
         {
+            if (category.Name == category.DisplayOrder.ToString())
+                ModelState.AddModelError("name", "The Display Order cannot exactly match the Name.");
+
             if (ModelState.IsValid)
             {
                 _dbContext.Categories.Add(category);
                 _dbContext.SaveChanges();
+                TempData["success"] = "Category created successfully";
                 return RedirectToAction("Index"); 
             }
             return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+                return NotFound();
+
+            Category? category = _dbContext.Categories.FirstOrDefault(x => x.Id == id);
+            if (category == null) return NotFound();
+
+            return View(category);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _dbContext.Categories.Update(category);
+                _dbContext.SaveChanges();
+                TempData["success"] = "Category updated successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+                return NotFound();
+
+            Category? category = _dbContext.Categories.FirstOrDefault(x => x.Id == id);
+            if (category == null) return NotFound();
+
+            return View(category);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Category? category = _dbContext.Categories.FirstOrDefault(x => x.Id == id);
+            if (category == null) return NotFound();
+
+            _dbContext.Categories.Remove(category);
+            _dbContext.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
+            return RedirectToAction("Index");
         }
     }
 }
