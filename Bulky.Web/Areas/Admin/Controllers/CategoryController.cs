30fec2b01c80d2f17422b622e5144640d5ba5bce
BehruzbekUmarov <bekhruz476@gmail.com>
2024-09-27 19:34:49 -0800

    Area is added

--- Bulky.Web/Areas/Admin/Controllers/CategoryController.cs
+++ Bulky.Web/Areas/Admin/Controllers/CategoryController.cs
@@ -2,8 +2,9 @@
 using Bulky.Models;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Bulky.Web.Controllers
+namespace Bulky.Web.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class CategoryController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
@@ -35,14 +36,14 @@ namespace Bulky.Web.Controllers
                 _unitOfWork.Category.Add(category);
                 _unitOfWork.Save();
                 TempData["success"] = "Category created successfully";
-                return RedirectToAction("Index"); 
+                return RedirectToAction("Index");
             }
             return View();
         }
 
         public IActionResult Edit(int? id)
         {
-            if(id == null || id == 0)
+            if (id == null || id == 0)
                 return NotFound();
 
             Category? category = _unitOfWork.Category.Get(u => u.Id == id);
--- Bulky.Web/Areas/Customer/Controllers/HomeController.cs
+++ Bulky.Web/Areas/Customer/Controllers/HomeController.cs
@@ -1,10 +1,10 @@
-
 using Bulky.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
-namespace Bulky.Web.Controllers
+namespace Bulky.Web.Areas.Customer.Controllers
 {
+    [Area("Customer")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
