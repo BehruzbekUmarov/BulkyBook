1f6308682152093e7aa1ad83234f235b13aa5815
BehruzbekUmarov <bekhruz476@gmail.com>
2024-10-08 07:59:40 -0800

    shoppingCart Repository is added

--- Bulky.DataAccess/Repository/UnitOfWork.cs
+++ Bulky.DataAccess/Repository/UnitOfWork.cs
@@ -9,6 +9,7 @@ namespace Bulky.DataAccess.Repository
         public ICategoryRepository Category { get; private set; }
         public IProductRepository Product { get; private set; }
         public ICompanyRepository Company { get; private set; }
+        public IShoppingCartRepository ShoppingCart { get; private set; }
 
         public UnitOfWork(ApplicationDbContext context)
         {
@@ -16,6 +17,7 @@ namespace Bulky.DataAccess.Repository
             Category = new CategoryRepository(_context);
             Product = new ProductRepository(_context);
             Company = new CompanyRepository(_context);
+            ShoppingCart = new ShoppingCartRepository(_context);
         }
       
 
